96212b8b498fcfd34bea51ed67da45e351363503
maddy-hops <sophie.grinevich@gmail.com>
2021-11-18 16:21:56 +0700

    make period volatile so we can edit it

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -3,18 +3,22 @@
 #![no_std]
 
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
 	let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
-	let period = 50_u16;
+
+	let mut period = 50_u16;
+	let v_period = Volatile::new(&mut period);
+
 	loop {
 		for i in 0..8 {
 			let next = (i + 1) % 8;
 			leds[next].on().ok();
-			delay.delay_ms(period);
+			delay.delay_ms(v_period.read());
 			leds[i].off().ok();
-			delay.delay_ms(period);
+			delay.delay_ms(v_period.read());
 		}
 	}
 }
